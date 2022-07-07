f727b5fa42851eb839636cc36e85fd89b182568a
Prat2030 <pratyushjindal2003@gmail.com>
2022-07-07 19:54:19 +0530

    basic implementation of firestore services like snapshots,etc.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (ctx, item) => Container(
-                padding: EdgeInsets.all(8),
-                child: Text('This is a chat message'),
-              )),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/iXiiEyfosMFcf7FBUpED/messages')
+            .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+              itemCount: documents.length,
+              itemBuilder: (ctx, index) => Container(
+                    padding: EdgeInsets.all(8),
+                    child: Text(documents[index]['text']),
+                  ));
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/iXiiEyfosMFcf7FBUpED/messages')
+              .add({
+            'text': 'This was added after clicking the button',
+          });
+        },
+      ),
     );
   }
 }
