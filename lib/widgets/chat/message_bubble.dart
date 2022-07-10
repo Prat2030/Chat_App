0db75bbcb5300baa3621bcc00332605a6380125d
Prat2030 <pratyushjindal2003@gmail.com>
2022-07-10 18:16:58 +0530

    implemented some styling and display depending on which side has sent the message to other side.

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -2,16 +2,23 @@ import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
   final String message;
-  MessageBubble({this.message});
+  final bool isMe;
+  MessageBubble({this.message, this.isMe});
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
         Container(
           decoration: BoxDecoration(
-            color: Theme.of(context).accentColor,
-            borderRadius: BorderRadius.circular(12),
+            color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12),
+              topRight: Radius.circular(12),
+              bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
+              bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
+            ),
           ),
           width: 140,
           padding: EdgeInsets.symmetric(
@@ -26,7 +33,9 @@ class MessageBubble extends StatelessWidget {
             message,
             style: TextStyle(
               // fontSize: 16,
-              color: Theme.of(context).accentTextTheme.headline1.color,
+              color: isMe
+                  ? Colors.black
+                  : Theme.of(context).accentTextTheme.headline1.color,
             ),
           ),
         ),
--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -1,28 +1,39 @@
 import 'package:chat_app/widgets/chat/message_bubble.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Firestore.instance
-          .collection('chat')
-          .orderBy('createdAt', descending: true)
-          .snapshots(),
-      builder: (ctx, streamSnapshot) {
-        if (streamSnapshot.connectionState == ConnectionState.waiting) {
+    return FutureBuilder(
+      future: FirebaseAuth.instance.currentUser(),
+      builder: (ctx, futureSnapshot) {
+        if (futureSnapshot.connectionState == ConnectionState.waiting)
           return Center(
             child: CircularProgressIndicator(),
           );
-        }
-        final chatDocs = streamSnapshot.data.documents;
-        return ListView.builder(
-          reverse: true,
-          itemCount: chatDocs.length,
-          itemBuilder: (ctx, index) =>
-              MessageBubble(message: chatDocs[index]['text']),
-        );
+        return StreamBuilder(
+            stream: Firestore.instance
+                .collection('chat')
+                .orderBy('createdAt', descending: true)
+                .snapshots(),
+            builder: (ctx, streamSnapshot) {
+              if (streamSnapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              final chatDocs = streamSnapshot.data.documents;
+              return ListView.builder(
+                reverse: true,
+                itemCount: chatDocs.length,
+                itemBuilder: (ctx, index) => MessageBubble(
+                  message: chatDocs[index]['text'],
+                  isMe: chatDocs[index]['userId'] == futureSnapshot.data.uid,
+                ),
+              );
+            });
       },
     );
   }
