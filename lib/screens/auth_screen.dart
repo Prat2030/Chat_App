96b183f98d100a250ca9c27f183e369440c12973
Prat2030 <pratyushjindal2003@gmail.com>
2022-07-15 13:58:29 +0530

    added image upload feature via firebase storage

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -27,13 +28,6 @@ class _AuthScreenState extends State<AuthScreen> {
     BuildContext ctx,
   ) async {
     AuthResult authResult;
-    // if (isLogin) {
-    //   authResult = await _auth.signInWithEmailAndPassword(
-    //       email: email, password: password);
-    // } else {
-    //   authResult = await _auth.createUserWithEmailAndPassword(
-    //       email: email, password: password);
-
     try {
       setState(() {
         _isLoading = true;
@@ -44,6 +38,14 @@ class _AuthScreenState extends State<AuthScreen> {
       } else {
         authResult = await _auth.createUserWithEmailAndPassword(
             email: email, password: password);
+
+        final ref = FirebaseStorage.instance
+            .ref()
+            .child('user_images')
+            .child(authResult.user.uid + '.jpg');
+
+        await ref.putFile(image).onComplete;
+
         await Firestore.instance
             .collection('users')
             .document(authResult.user.uid)
